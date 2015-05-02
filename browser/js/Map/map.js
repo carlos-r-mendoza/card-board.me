b03a94b73ac8f0fd31157592e9064f45d8862aaf
Ryan Bronz <ryanbronz@Ryans-MacBook-Pro.local>
2015-05-02 01:12:00 -0400

    Got Graph to show up

--- browser/js/Map/map.js
+++ browser/js/Map/map.js
@@ -19,6 +19,8 @@ app.factory('SearchFactory', function($http){
 });
 
 app.controller('SearchController', function($scope, SearchFactory) {
+	console.log("Reached Controller");
+	//gt
 	$scope.search={};
 	$scope.search.text="";
 	$scope.search.language="";
@@ -33,39 +35,47 @@ app.controller('SearchController', function($scope, SearchFactory) {
 			console.log(error);
 		}
 
+
 	};
-	// //Load the Visualization API and the piechart package.
- //      google.load('visualization', '1.0', {'packages':['corechart']});
-
- //      // Set a callback to run when the Google Visualization API is loaded.
- //      google.setOnLoadCallback(drawChart);
-
- //      // Callback that creates and populates a data table,
- //      // instantiates the pie chart, passes in the data and
- //      // draws it.
- //      function drawChart() {
-
- //        // Create the data table.
- //        var data = new google.visualization.DataTable();
- //        data.addColumn('string', 'Topping');
- //        data.addColumn('number', 'Slices');
- //        data.addRows([
- //          ['Mushrooms', 3],
- //          ['Onions', 1],
- //          ['Olives', 1],
- //          ['Zucchini', 1],
- //          ['Pepperoni', 2]
- //        ]);
-
- //        // Set chart options
- //        var options = {'title':'How Much Pizza I Ate Last Night',
- //                       'width':400,
- //                       'height':300};
-
- //        // Instantiate and draw our chart, passing in some options.
- //        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
- //        chart.draw(data, options);
- //      }
-
-
 });
+  app.controller("BarCtrl", function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+});
+	//Load the Visualization API and the piechart package.
+      //google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // // Set a callback to run when the Google Visualization API is loaded.
+      // google.setOnLoadCallback(drawChart);
+
+      // // Callback that creates and populates a data table,
+      // // instantiates the pie chart, passes in the data and
+      // // draws it.
+      // function drawChart() {
+
+      //   // Create the data table.
+      //   var data = new google.visualization.DataTable();
+      //   data.addColumn('string', 'Topping');
+      //   data.addColumn('number', 'Slices');
+      //   data.addRows([
+      //     ['Mushrooms', 3],
+      //     ['Onions', 1],
+      //     ['Olives', 1],
+      //     ['Zucchini', 1],
+      //     ['Pepperoni', 2]
+      //   ]);
+
+      //   // Set chart options
+      //   var options = {'title':'How Much Pizza I Ate Last Night',
+      //                  'width':400,
+      //                  'height':300};
+
+      //   // Instantiate and draw our chart, passing in some options.
+      //   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      //   chart.draw(data, options);
+      // }
