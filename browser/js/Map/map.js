6a3be303d43c9042251c780436f7b693625b30a5
Ryan Bronz <ryanbronz@Ryans-MacBook-Pro.local>
2015-05-02 15:02:45 -0400

    Graphs responding to Data Changes

--- browser/js/Map/map.js
+++ browser/js/Map/map.js
@@ -17,8 +17,11 @@ app.factory('SearchFactory', function($http){
 	};
 
 });
+app.factory('DataShare', function () {
+    return {};
+});
 
-app.controller('SearchController', function($scope, SearchFactory) {
+app.controller('SearchController', function($scope, SearchFactory, DataShare) {
 	console.log("Reached Controller");
 	//gt
 	$scope.search={};
@@ -30,6 +33,17 @@ app.controller('SearchController', function($scope, SearchFactory) {
 		SearchFactory.getSearchResults(text,language).then(fulfilled,rejected);
 		function fulfilled(results){
 			$scope.searchResults=results.data.items;
+      DataShare.labels=[]
+      DataShare.data=[[]];
+      for (var i=0;i<5;i++){
+        DataShare.labels.push($scope.searchResults[i].name);
+        DataShare.data[0].push($scope.searchResults[i].stargazers_count);
+      }
+     //console.log("DATASHARE", DataShare);
+      //DataShare.labels=[$scope.searchResults[0].name];
+      //DataShare.data=[[$scope.searchResults[0].stargazers_count]];
+      console.log("Yeehaw", DataShare.labels, DataShare.data);
+      //$scope.$digest();
 		}
 		function rejected(error){
 			console.log(error);
@@ -38,13 +52,16 @@ app.controller('SearchController', function($scope, SearchFactory) {
 
 	};
 });
-  app.controller("BarCtrl", function ($scope) {
-  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
+  app.controller("BarCtrl", function ($scope, DataShare) {
+  //$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  //$scope.series = ['Series A'];
 
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  //$scope.data = [
+     //[65, 59, 80, 81, 56, 55, 75]];
+     //console.log("DATASHARE", DataShare);
+
+
+  $scope.dataShare = DataShare
+  //$scope.$digest();
 });
 	
