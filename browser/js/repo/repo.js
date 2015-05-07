1746e6b033f2a3369905544aa6e578f0627ec529
ksliao <katrina.s.liao@gmail.com>
2015-05-07 17:04:56 -0400

    refactored repo factories and intialized ng-sortable

--- browser/js/repo/repo.js
+++ browser/js/repo/repo.js
@@ -15,74 +15,32 @@ app.factory('RepoInfoFactory', function($http){
 			return $http.get('api/repo/repo/' + repoName).then(function(repoInfo){
 				return repoInfo;
 			});
-		}
-	};
-})
-
-app.factory('RepoCollaboratorsFactory', function($http){
-				
-
-	return {
+		},
 		getRepoCollaborators: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/collaborators").then(function(repoCollaborators){
 				return repoCollaborators;
 			});
-		}
-	};
-})
-
-app.factory('RepoCommitsFactory', function($http){
-				
-
-	return {
+		},
 		getRepoCommits: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/commits").then(function(repoCommits){
 				return repoCommits;
 			});
-		}
-	};
-})
-
-app.factory('RepoStatsCodeFrequencyFactory', function($http){
-				
-
-	return {
+		},
 		getStatsCodeFrequency: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/statsCodeFrequency").then(function(statsCodeFrequency){
 				return statsCodeFrequency;
 			});
-		}
-	};
-})
-
-app.factory('RepoStatsCommitActivityFactory', function($http){
-				
-
-	return {
+		},
 		getStatsCommitActivity: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/statsCommitActivity").then(function(statsCommitActivity){
 				return statsCommitActivity;
 			});
-		}
-	};
-})
-
-app.factory('RepoStatsContributorsFactory', function($http){
-				
-
-	return {
+		},
 		getStatsContributors: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/statsContributors").then(function(statsContributors){
 				return statsContributors;
 			});
-		}
-	};
-})
-
-app.factory('RepoStatsParticipationFactory', function($http){
-				
-
-	return {
+		},
 		getStatsParticipation: function (repoName){
 			return $http.get('api/repo/repo/' + repoName +"/statsParticipation").then(function(statsParticipation){
 				return statsParticipation;
@@ -92,10 +50,9 @@ app.factory('RepoStatsParticipationFactory', function($http){
 })
 
 
-app.controller('RepoShowController', function($scope, $stateParams, $state, RepoInfoFactory, RepoCollaboratorsFactory, RepoCommitsFactory, RepoStatsCodeFrequencyFactory, RepoStatsCommitActivityFactory, RepoStatsContributorsFactory, RepoStatsParticipationFactory){
-	$scope.hello = "Welcomes";
-	console.log("WHAT", $stateParams)
 
+app.controller('RepoShowController', function($scope, $stateParams, $state, RepoInfoFactory){
+	$scope.hello = "Welcomes";
 
 		function repoInfoFulfilled(repoInfo) {
 		
@@ -113,18 +70,13 @@ app.controller('RepoShowController', function($scope, $stateParams, $state, Repo
 		// 	repoObj.forks = repo.forks_count;
 
 		// 	$scope.profileRepos.push(repoObj);
-		
 		}
 
 		function repoCollaboratorsFulfilled(repoCollaborators) {
-		
-		console.log("HARESEGDFAGA")
 		 $scope.collaborators = repoCollaborators;
 		}
 
 		function repoCommitsFulfilled(repoCommits) {
-		
-		console.log("HARESEGDFAGA")
 		 $scope.commits = repoCommits;
 		}
 
@@ -133,7 +85,6 @@ app.controller('RepoShowController', function($scope, $stateParams, $state, Repo
 		}
 
 		function statsCommitActivityFulfilled(statsCommitActivity) {
-			console.log("STATTS", statsCommitActivity )
 			$scope.statsCommit = statsCommitActivity.data[statsCommitActivity.data.length-1];
 		}
 
@@ -152,12 +103,12 @@ app.controller('RepoShowController', function($scope, $stateParams, $state, Repo
 			console.log("YO")
 		
 		RepoInfoFactory.getRepoInfo($stateParams.name).then(repoInfoFulfilled, rejected);
-		RepoCollaboratorsFactory.getRepoCollaborators($stateParams.name).then(repoCollaboratorsFulfilled, rejected);
-		RepoCommitsFactory.getRepoCommits($stateParams.name).then(repoCommitsFulfilled, rejected);
-		RepoStatsCodeFrequencyFactory.getStatsCodeFrequency($stateParams.name).then(statsCodeFrequencyFulfilled, rejected);
-		RepoStatsCommitActivityFactory.getStatsCommitActivity($stateParams.name).then(statsCommitActivityFulfilled, rejected);
-		RepoStatsContributorsFactory.getStatsContributors($stateParams.name).then(statsContributorsFulfilled, rejected);
-		RepoStatsParticipationFactory.getStatsParticipation($stateParams.name).then(statsParticipationFulfilled, rejected);
+		RepoInfoFactory.getRepoCollaborators($stateParams.name).then(repoCollaboratorsFulfilled, rejected);
+		RepoInfoFactory.getRepoCommits($stateParams.name).then(repoCommitsFulfilled, rejected);
+		RepoInfoFactory.getStatsCodeFrequency($stateParams.name).then(statsCodeFrequencyFulfilled, rejected);
+		RepoInfoFactory.getStatsCommitActivity($stateParams.name).then(statsCommitActivityFulfilled, rejected);
+		RepoInfoFactory.getStatsContributors($stateParams.name).then(statsContributorsFulfilled, rejected);
+		RepoInfoFactory.getStatsParticipation($stateParams.name).then(statsParticipationFulfilled, rejected);
 
 
 
