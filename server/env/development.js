8df4130c8165b029b77dd802c5680a08265bc45e
carlos-r-mendoza <clos.mendoza@gmail.com>
2015-05-12 22:17:15 -0400

    gitignore modified. -Carlos

---
+++ server/env/development.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "GITHUB": {
+   "clientID": "",
+   "clientSecret": "",
+   "callbackURL": ""}
+ };
