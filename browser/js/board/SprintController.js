779824c8ffa26c5e769d80a3e507aa37c6928be9
carlos-r-mendoza <clos.mendoza@gmail.com>
2015-05-15 01:25:35 -0400

    board population now works! -Carlos

--- browser/js/board/SprintController.js
+++ browser/js/board/SprintController.js
@@ -36,7 +36,7 @@ app.controller('SprintController', function ($scope, $stateParams, BoardService,
 
     var sprint={
       "name": "Sprint Board",
-      "numberOfColumns": 4,
+      "numberOfColumns": 3, //change this number to match number of hardcoded columns 
       "columns": [
         {"name": "Open"},
         {"name": "In Progress"},
@@ -54,17 +54,19 @@ app.controller('SprintController', function ($scope, $stateParams, BoardService,
     function getLabelFeatures(repoLabels) {
       // console.log("RE")
       repoLabels.data.forEach(function(label) {
-        var featureName = label.name.split(" - ");
+        var labelName = label.name.split(" - ");
               // console.log("HERE", featureSplit);
 
-        if(featureName[0] === "Feature") {
+        if(labelName[0] === "Feature" && labelName[1]) {
           featureLabelsArr.push(repoLabels);
-          populateFeaturesColumn(featureName[1]);
-        } else if (featureName[0] === "Phase") {
-          createPhaseColumn(featureName[1]);
+          populateFeaturesColumn(labelName[1]);
+          //console.log(sprint)
+        } else if (labelName[0] === "Phase" && labelName[1] !== "In Progress") {
+          createPhaseColumn(labelName[1]);
         }
       });
       // $scope.sprintBoard = BoardService.sprintBoard(sprint);  
+      addPhasesToFeatures();
 
       RepoFactory.getRepoIssues($stateParams).then(addIssuesToPhases, rejected);
 
@@ -74,12 +76,12 @@ app.controller('SprintController', function ($scope, $stateParams, BoardService,
 
     function populateFeaturesColumn(featuresTitle) {
       sprint.features.push(
-        {"title": featuresTitle, //label
+        {"title": featuresTitle, //rows
           "details": "Feature",  //can delete
           "phases": [
-            {"name": "Open",
+            {"name": "Open", //colums
               "cards": []},
-            {"name": "In progress",
+            {"name": "In Progress",
               "cards": []},
             {"name": "Closed",
               "cards": []}
@@ -88,55 +90,97 @@ app.controller('SprintController', function ($scope, $stateParams, BoardService,
     }
 
     function createPhaseColumn(phase) {
-      console.log("sprintfeatures",sprint.features.phases);
-      console.log(phase);
+      // console.log("sprintfeatures",sprint.features.phases);
+      // console.log("AGFD", phase);
       sprint.columns.push(
         {"name": phase});
-      addPhasesToFeatures();
     }
 
     function addPhasesToFeatures() {
-      sprint.columns.forEach(function(column) {
+      for (var i = sprint.numberOfColumns; i <= sprint.columns.length-1; i++) { //2, 4
         sprint.features.forEach(function(feature){
-          feature.phases.push({"name": column, //Status Open
+          feature.phases.push({"name": sprint.columns[i].name, //Status Open
               "cards": []});
           });
-        });
+         }
       }
     
     function addIssuesToPhases(repoIssues) {
+      //sconsole.log("SSS",sprint)
       repoIssues.data.forEach(function(issue) {
       if(issue.labels.length > 0) {
+        issue.labels.sort();
         // console.log(issue)
       //var labels = issue.labels.sort();
       
-        for (var i = 0; i < issue.labels.length-1; i++) {
+        for (var i = 0; i <= issue.labels.length-1; i++) {
           sprint.features.forEach(function(feature) {
              //console.log("FEA", feature)
             if(issue.labels[i].name === "Feature - " + feature.title) {
               // console.log(labels);
 
-                feature.phases.forEach(function(phase){
+                for (var j = sprint.numberOfColumns; j <= feature.phases.length-1; j++) {
                               // console.log("LABEL", issue.labels[1], issue.title, phase.name.name)
+                              //console.log("FEa", feature.phases)
+                  //issue.labels.forEach(function(label){
+                    //console.log("LLL", label,  feature.phases[3].name)
 
-                  issue.labels.forEach(function(label){
+                    // if(label.name === "Phase - " + feature.phase[i].name) {
+                    //   console.log("AT PHASE");
+                    
+                    //   feature.phase.cards.push({"title": issue.title, //issue name
+                    //   "details": "Testing Card Details", //issue body
+                    //   "status": "Open"});
+                    //   console.log(phase.name);
 
-                    if(label.name === "Phase - " + phase.name.name) {
+                    // }
+            //         $scope.sprintBoard = BoardService.sprintBoard(sprint);                 
+
+                  // for (var k = 0; k < feature.phases.length-1; k++) {
+                  for (var k = i; k <= issue.labels.length-1; k++) {
+                    if(issue.labels[k].name === "Phase - " + "In Progress") {
                       //console.log("AT PHASE",phase);
                     
-                      phase.cards.push({"title": issue.title, //issue name
+                     feature.phases[1].cards.push({"title": issue.title, //issue name
                       "details": "Testing Card Details", //issue body
                       "status": "Open"});
-                      console.log(phase.name);
+                      //console.log(phase.name);
+                      break;
+                    }
+                    if(issue.labels[k].name === "Phase - " + feature.phases[j].name) {
+                      //console.log("AT PHASE",phase);
+                    
+                     feature.phases[j].cards.push({"title": issue.title, //issue name
+                      "details": "Testing Card Details", //issue body
+                      "status": "Open"});
+                      //console.log(phase.name);
+                      break;
+                    }
+                    if(k === issue.labels.length-1) {
+                      if(issue.state === "open") {
+                        console.log("AF", issue)
+                       feature.phases[0].cards.push({"title": issue.title, //issue name
+                        "details": "Testing Card Details", //issue body
+                        "status": "Open"});
+                      }
 
                     }
-            //         $scope.sprintBoard = BoardService.sprintBoard(sprint);  
+                    if(k === issue.labels.length-1) {
+                      if(issue.state === "closed") {
+                        console.log("AF", issue)
+                       feature.phases[2].cards.push({"title": issue.title, //issue name
+                        "details": "Testing Card Details", //issue body
+                        "status": "Open"});
+                      }
 
+                    }
 
-
-                  });
-
-                });
+                  }  
+                  break;
+                  // }
+               //});
+              }
+      
    
             }
           });
