a54a0734f92f0b0a6926432e65c0e13bfe51bb52
ksliao <katrina.s.liao@gmail.com>
2015-05-26 18:18:22 -0400

    deployment

--- server/env/production.js
+++ server/env/production.js
@@ -23,5 +23,10 @@ module.exports = {
         "clientID": process.env.GOOGLE_CLIENT_ID,
         "clientSecret": process.env.GOOGLE_CLIENT_SECRET,
         "callbackURL": process.env.CALLBACK_URL
+    },
+    "GITHUB": {
+        "clientID": "e77ce196780477b8e1f8",
+        "clientSecret": "5e4b905f1147b85b14be724b020e178a28045a8c",
+        "callbackURL": "http://localhost:1337/auth/github/callback"
     }
 };
